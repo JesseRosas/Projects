2a0e07c07fd181797216c55f1666a8546a5a1117
JesseRosas <Jessebrosas@yahoo.com>
2024-10-01 12:07:13 -0400

    Fixed Remaining Errors

--- src/TestDriver.java
+++ src/TestDriver.java
@@ -1,14 +1,16 @@
+// Jesse Rosas
+// Lab 4
 public class TestDriver {
     public static void main(String[] args) {
         Fahrenheit tempF = new Fahrenheit(65);
         Celsius tempC = new Celsius(-2);
 
         System.out.println(tempF.belowFreezing());
-        System.out.println(tempF.swimmingWeather);
-        System.out.println(tempF.toString);
+        System.out.println(tempF.swimmingWeather());
+        System.out.println(tempF.toString());
 
         System.out.println(tempC.belowFreezing());
-        System.out.println(tempC.swimmingWeather);
-        System.out.println(tempC.toString);
+        System.out.println(tempC.swimmingWeather());
+        System.out.println(tempC.toString());
     }
 }
